01fb320cc6d96948cce033d6505f39fec815345f
zohirx <den.aymen.zohir@gmail.com>
2025-11-20 13:51:31 +0100

    exercice 1 : pattern builder

--- src/main/java/com/polytech/App.java
+++ src/main/java/com/polytech/App.java
@@ -1,5 +1,7 @@
 package com.polytech;
 
+import com.polytech.tp.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Cours cours1 = new CoursBuilder()
+                .setMatiere("Génie Logiciel")
+                .setEnseignant("Dr. Dupont")
+                .setSalle("A101")
+                .setDate("2024-11-20")
+                .setHeureDebut("10:00")
+                .setEstOptionnel(false)
+                .setNiveau("Master")
+                .setNecessiteProjecteur(true)
+                .build();
+        System.out.println(cours1.getMatiere());
     }
 }
--- src/main/java/com/polytech/tp/CoursBuilder.java
+++ src/main/java/com/polytech/tp/CoursBuilder.java
@@ -1,14 +1,59 @@
 package com.polytech.tp;
 
 public class CoursBuilder {
-    // TODO: Implémenter le pattern Builder
-    
-    
-    public CoursBuilder setMatiere(String matiere) { return this; }
-    public CoursBuilder setEnseignant(String enseignant) { return this; }
-    
+
+    private String matiere;
+    private String enseignant;
+    private String salle;
+    private String date;
+    private String heureDebut;
+    private boolean estOptionnel;
+    private String niveau;
+    private boolean necessiteProjecteur;
+
+
+    public CoursBuilder setMatiere(String matiere) {
+        this.matiere = matiere;
+        return this;
+    }
+
+    public CoursBuilder setEnseignant(String enseignant) {
+        this.enseignant = enseignant;
+        return this;
+    }
+
+    public CoursBuilder setSalle(String salle) {
+        this.salle = salle;
+        return this;
+    }
+
+    public CoursBuilder setDate(String date) {
+        this.date = date;
+        return this;
+    }
+
+    public CoursBuilder setHeureDebut(String heureDebut) {
+        this.heureDebut = heureDebut;
+        return this;
+    }
+
+    public CoursBuilder setEstOptionnel(boolean estOptionnel) {
+        this.estOptionnel = estOptionnel;
+        return this;
+    }
+
+    public CoursBuilder setNiveau(String niveau) {
+        this.niveau = niveau;
+        return this;
+    }
+
+    public CoursBuilder setNecessiteProjecteur(boolean necessiteProjecteur) {
+        this.necessiteProjecteur = necessiteProjecteur;
+        return this;
+    }
     
     public Cours build() {
-        return null;
+        return new Cours(matiere, enseignant, salle, date,
+                heureDebut, estOptionnel, niveau, necessiteProjecteur);
     }
 }
